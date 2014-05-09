8f0ed52e603b2d32dab0f9eb64a36618ce420d1b
Guilherme Côrtes <guilhermecortes@gmail.com>
2014-05-09 20:15:54 -0300

    Retirada da chave estrangeira da tabela grupo
    Retirou a chave estrangeira pois estava dando ‘pau’.
    Adicionado virgula para execução de todas as tabelas

--- src/schema.sql
+++ src/schema.sql
@@ -2,14 +2,14 @@ CREATE TABLE oferta (
 	cod_oferta int not null CONSTRAINT PK_OFERTA PRIMARY KEY,
 	nome_user varchar(50),
 	cod_produto INT 
-)
+);
 
 CREATE TABLE proposta (
 	cod_proposta int not null CONSTRAINT PK_proposta PRIMARY KEY,
 	nome_user VARCHAR(50),
 	desejo varchar(50)
 
-)
+);
 
 
 CREATE TABLE grupo (
@@ -17,6 +17,6 @@ CREATE TABLE grupo (
 	titulo VARCHAR(50),
 	descricao VARCHAR(80),
 	fase int,
-	cod_proposta INT constraint FK_grupo_proposta FOREIGN KEY REFERENCES cod_proposta (proposta)
+	cod_proposta INT
 	
 )
